dac757eeb404d9b414a29086f599a9ab43f2a57b
todakou <todmail1124@gmail.com>
2020-11-18 15:30:29 +0900

    既読機能サーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,5 +10,17 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     reddirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.uodate(checked: true)
+    end
+
+    item = Post.find(params [:id])
+    render json: { post: item }
+  end
 end
 
